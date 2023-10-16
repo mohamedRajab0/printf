22007e916cb6330a4454d41448629f4c0675ff7e
mohanedRajab0 <elzaimmohamed@gmail.com>
2023-10-16 09:15:00 -0700

    updata

--- main.h
+++ main.h
@@ -1,1 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
 
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <limits.h>
+
+int _putchar(char c);
+int print_digit(long n, int base);
+int print_str(char *s);
+int print_format(char specifier, va_list ap);
+int _printf(const char *format, ...);
+
+#endif
